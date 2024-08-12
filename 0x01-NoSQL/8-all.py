141893e669344a136c6bf1c27fdbb4763e6f7676
SirJupiter <jupitertoby@gmail.com>
2024-08-12 18:37:26 +0100

    8. List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module"""
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+    collection = mongo_collection.find()
+    if not collection:
+        return []
+    return collection
